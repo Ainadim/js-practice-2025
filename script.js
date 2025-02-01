dfeccfaa0b3bfd967716b202812b5dfc8c8659cf
Arafin Islam <nadimface@gmail.com>
2025-02-01 08:56:31 +0600

    aad stop fun

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
+let interval;
+let pos = 0;
 function moving() {
     const box = document.getElementById("box");
-    let pos = 0;
-
-
-    const interval =  setInterval(animation, 10);
+  
+    
+    interval =  setInterval(animation, 10);
+    
 
     function animation() {
-        if (pos < 350 && pos1 < 350) {
+        if (pos < 350) {
             pos++;
             box.style.top = pos + "px";
             box.style.left = pos + "px";
-        } else{
-            clearInterval(interval)
+        }
+         else{
+            clearInterval(interval);
         }
     }
 }
+
+document.getElementById("stopButton").addEventListener("click", function () {
+    clearInterval(interval);
+})
